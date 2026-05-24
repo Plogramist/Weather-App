2de0637ca040ccbddffc419409efbcdd032f1c93
Plogramist <coffymacochino@gmail.com>
2026-05-24 19:53:35 +0200

    Update weather display styles and enhance city/date information

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const searchForm = document.querySelector(".search-container");
+const cityInput = document.querySelector("#city-input");
+
+const cityName = document.querySelector("#city-name");
+const dateElement = document.querySelector("#date");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description");
+
+const feelsLike = document.querySelector("#feels-like");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind-speed");
+const pressure = document.querySelector("#pressure");
